7286f72e6af7088c40b5910a00890eca7d6442ac
kurra sreedhar <arrukjay888@gmail.com>
2025-02-02 23:10:42 +0530

    complete

--- src/UTILS/Contants.js
+++ src/UTILS/Contants.js
@@ -2,8 +2,8 @@ export  const  options = {
     method: 'GET',
     headers: {
       accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4MDhkNzM5Y2RlNTZlOGM0NWY1Nzk2YjU2N2RiNTZmYyIsIm5iZiI6MTcyNjg0MDQxOC45MTY5OTk4LCJzdWIiOiI2NmVkN2U2MjgyZjU2MThkOTBhZGViNTAiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.wQz_dspArB17JRVYpxYunRhSE-BtUkm4AMGPs9NUIX8'
+      Authorization: 'Bearer'
     }
   };
    export const CDN_IMG="https://image.tmdb.org/t/p/w200";
-   export const AIKEY='AIzaSyD9jqj9AG52y4Uqg20DXj4wH07Vd8pDTNc'
+   export const AIKEY=''
